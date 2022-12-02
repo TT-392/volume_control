4fc786cae22cb2736a6288963f9ea14187ca0850
TT-392 <TT-392@protonmail.com>
2022-12-02 17:38:21 +0100

    Multiplexing input working

--- code/src/main.c
+++ code/src/main.c
@@ -2,59 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
-#include "matrix.h"
+#include "pico/bootrom.h"
+#include "pico/multicore.h"
+#include "monitor_keys.h"
 
 int main() {
     stdio_init_all();
 
     while (!stdio_usb_connected());
     printf("USB connected\n");
-    gpio_pull_down(13);
-    gpio_set_dir(13, false);
-    gpio_init(9);
-    gpio_set_dir(9, true);
-    gpio_put(9, true);
+
+    multicore_launch_core1(monitor_keys);
 
     while (1) {
-        if (gpio_get(13)) {
-            printf("F\n");
-        }
+        //if (gpio_get(11)) {
+        //    printf("L\n");
+        //    reset_usb_boot(0, 0);
+        //}
     }
- 
-//    while (1) {
-//        char display[10][72] = {0};
-//
-//        for (int y = 0; y < 10; y++) {
-//            for (int x = 0; x < 72; x++) {
-//                display[y][x] = (((x / 5) % 2) ^ ((y / 5) % 2)) * 0x07;
-//            }
-//        }
-//
-//        matrix_put(display);
-//        matrix_update();
-//        sleep_ms(1000);
-//
-//        memset(display, 0x0, sizeof(display));
-//        matrix_put(display);
-//        matrix_update();
-//
-//        sleep_ms(1000);
-//    }
-//
-//
-//    while (1);
-
-    while (1) {
-        matrix_set_pixel(5, 5, 0xfe);
-        sleep_ms(1000);
-        matrix_fill_rect(0, 0, 5, 5, 0xfe);
-        sleep_ms(1000);
-        matrix_clear();
-        matrix_fill_rect(0, 0, 0x47, 9, 0xfe);
-        sleep_ms(1000);
-        matrix_clear();
-        sleep_ms(1000);
-    }
-
 }
 
---
+++ code/src/monitor_keys.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pins_left.h"
+#include "pico/bootrom.h"
+
+
+void monitor_keys() {
+    uint cols[] = {PIN_COL0, PIN_COL1, PIN_COL2, PIN_COL3, PIN_COL4, PIN_COL5, PIN_COL6};
+    uint rows[] = {PIN_ROW0, PIN_ROW1, PIN_ROW2, PIN_ROW3, PIN_ROW4};
+
+    printf("start key monitoring\n");
+    
+    for (int i = 0; i < sizeof(cols) / sizeof(uint); i++) {
+        gpio_init(cols[i]);
+        gpio_pull_up(cols[i]);
+        gpio_set_dir(cols[i], 0);
+    }
+
+    for (int i = 0; i < sizeof(rows) / sizeof(uint); i++) {
+        gpio_init(rows[i]);
+        gpio_set_dir(rows[i], 1);
+        gpio_put(rows[i], 0);
+    }
+
+    while (1) {
+        for (int r = 0; r < sizeof(rows) / sizeof(uint); r++) {
+            gpio_put(rows[r], 0);
+
+            sleep_us(1);
+            for (int c = 0; c < sizeof(cols) / sizeof(uint); c++) {
+                if (!gpio_get(cols[c])) {
+                    printf("%i %i\n", r, c);
+                    if (r == 2 && c == 0)
+                        reset_usb_boot(0, 0);
+                }
+            }
+            sleep_us(1);
+            gpio_put(rows[r], 1);
+
+
+            sleep_us(1);
+        }
+    }
+}
+
