a2a7b42a2d1f0cd607fafd75177ba41634bbb4a2
TT-392 <TT-392@protonmail.com>
2023-05-04 16:35:33 +0200

    volume control first commit

--- code/src/main.c
+++ code/src/main.c
@@ -11,44 +11,90 @@
 #include "defines.h"
 #include "trrs.h"
 #include "usb.h"
+#define printf cdc_printf
 
 int main() {
     usb_init();
-    trrs_init();
-
-    side_t defines = get_defines();
 
     uint8_t pressed_keys[8] = {};
+    gpio_init(25);
+    gpio_set_dir(25, 1);
+    gpio_put(25, 1);
 
 
-    multicore_launch_core1(monitor_keys);
+    gpio_init(18);
+    gpio_pull_up(18);
+    gpio_set_dir(18, 0);
+    gpio_init(17);
+    gpio_pull_up(17);
+    gpio_set_dir(17, 0);
+    gpio_init(16);
+    gpio_pull_up(16);
+    gpio_set_dir(16, 0);
+
+
+    bool last_state = gpio_get(16);
+
+    int i = 0;
+    bool clear = false;
+
 
     while (true) {
-        bool master = usb_connected();
-        
-        if (matrix_monitor_event_available()) {
-            matrix_event_t matrix_event = matrix_monitor_get_event();
-            uint8_t mapped_key = defines.keymap[matrix_event.row][matrix_event.col];
+        bool new_state = gpio_get(16);
 
-            if (master) {
-                process_input(matrix_event.action, mapped_key);
-
-            } else {
-                trrs_send(hamming_16_11_encode(0xffff & (matrix_event.action | mapped_key)));
-            }
-
-        }
-
-        if (master) {
-            if (trrs_data_available()) {
-                uint16_t packet = trrs_read();
-                packet = hamming_16_11_decode(packet);
-
-                if (packet != 0) {
-                    process_input(packet & 0xff00, packet & 0xff);
+        if (last_state != new_state) {
+            if (last_state == 0 && new_state == 1) {
+                if (new_state != gpio_get(17)) {
+                    keyboard_update(0, (uint8_t[6]){HID_KEY_VOLUME_DOWN});
+                } else {
+                    keyboard_update(0, (uint8_t[6]){HID_KEY_VOLUME_UP});
                 }
             }
+
+            sleep_ms(10);
+            keyboard_update(0, (uint8_t[6]){0});
         }
+
+        if (!gpio_get(18)) {
+            keyboard_update(0, (uint8_t[6]){HID_KEY_MUTE});
+            sleep_ms(100);
+            clear = true;
+        } else if (clear) {
+            clear = false;
+            keyboard_update(0, (uint8_t[6]){0});
+        }
+
+        last_state = new_state;
+
+
+
+
+
+        //bool master = usb_connected();
+        //
+        //if (matrix_monitor_event_available()) {
+        //    matrix_event_t matrix_event = matrix_monitor_get_event();
+        //    uint8_t mapped_key = defines.keymap[matrix_event.row][matrix_event.col];
+
+        //    if (master) {
+        //        process_input(matrix_event.action, mapped_key);
+
+        //    } else {
+        //        trrs_send(hamming_17_11_encode(0xffff & (matrix_event.action | mapped_key)));
+        //    }
+
+        //}
+
+        //if (master) {
+        //    if (trrs_data_available()) {
+        //        uint17_t packet = trrs_read();
+        //        packet = hamming_17_11_decode(packet);
+
+        //        if (packet != 0) {
+        //            process_input(packet & 0xff00, packet & 0xff);
+        //        }
+        //    }
+        //}
 
         usb_task();
    }
